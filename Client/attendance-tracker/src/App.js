b4fc17413030f8d69e286e0b08f66323cff470dc
Evgeny Borisov <45000799+Evborisov@users.noreply.github.com>
2019-08-20 11:26:24 +0200

    Evgeny   structure (#3)
    * Add files via upload
    
    Project structure:
    Client folder - React 
    Backend folder - for Back-end
    
    * Add files via upload
    
    * Add files via upload
    
    Added fetching from the server

--- Client/attendance-tracker/src/App.js
+++ Client/attendance-tracker/src/App.js
@@ -1,10 +1,63 @@
+<<<<<<< HEAD
+import React, { Component } from 'react';
+=======
 import React from 'react';
+>>>>>>> master
 import Homepage from "./components/Homepage";
 import Login from "./components/Login";
 import Registration from "./components/Registration";
 import { Switch, Route } from "react-router-dom";
 import './App.css';
 
+<<<<<<< HEAD
+class App extends Component {
+  state = {
+    data: ""
+  };
+
+
+  // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from index.js
+  callBackendAPI = async () => {
+    const response = await fetch("http://localhost:5000/express_backend");
+    const body = await response.json();
+    console.log(body);
+    this.setState({ data: 'responce from the server' });
+    console.log(this.state);
+
+    if (response.status !== 200) {
+      throw Error(body.message)
+    }
+    return body;
+    ;
+  };
+
+  componentDidMount() {
+    // Call our fetch function below once the component mounts
+    this.callBackendAPI()
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello from App</h1>
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route path="/login" component={Login} />
+          <Route path="/registration" component={Registration} />
+        </Switch>
+        <p>server get should be here {this.state.data} </p>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
+
+
+
+=======
 function App() {
   return (
     <div>
@@ -20,3 +73,4 @@ function App() {
 }
 
 export default App;
+>>>>>>> master
