d4095af5d05f0b36b58e498f113787ea89887225
khadog <abodakhadog94@gmail.com>
2019-08-21 15:54:22 +0200

    define the swiperHandler func

--- Frontend/src/components/Register/Register.js
+++ Frontend/src/components/Register/Register.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import Slider from './Slider';
 import './Slider.css';
 
-export default function Registration() {
-  return (  
-    <div>
-      <div> Hello </div>
-    </div>
-  )
+export default class Registration extends React.Component {
+	state = {
+		status: ''
+	};
+	// This func is for selecting the active class in swiper and to picking up the property (alt) and updating the status in the state
+	swiperHandler = () => {
+		let activeImg = document.querySelector('.swiper-slide-active');
+		this.setState({ status: activeImg.alt });
+	};
+	render() {
+		return (
+			<div>
+				<Slider swiperHandler={this.swiperHandler} />
+				<form>
+					<h1>{this.state.status}</h1>
+					<input type="text" placeholder="Name" />
+					<input type="email" placeholder="Email" />
+					{this.state.status === 'Observer' ? (
+						<React.Fragment>
+							<input type="text" placeholder="Class" /> <input type="text" placeholder="School" />{' '}
+						</React.Fragment>
+					) : null}
+				</form>
+			</div>
+		);
+	}
 }
--- Frontend/src/components/Register/Slider.js
+++ Frontend/src/components/Register/Slider.js
@@ -1,43 +1,33 @@
 import React from 'react';
-import Swiper from 'react-id-swiper'
+import Swiper from 'react-id-swiper';
 
-const Slide = () => {
-    const params = {
-        pagination: {
-            el: '.swiper-pagination',
-            type: 'bullets',
-            clickable: true,
-
-        },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev'
-        },
-        slidesPerView: 'auto',
-        spaceBetween: 100,
-        loop: true,
-        centeredSlides: true,
-    }
-    return (
-        // <Swiper {...params}>
-        //     <img src='./img/attendee.png' alt="attendee" /><p>attendee</p>
-        //     <img src='./img/trainer.png' alt="trainer" /><p>trainer</p>
-        //     <img src='./img/management.png' alt="manager" /><p>management</p>
-        //     <img src='./img/observer.png' alt="observer" /><p>observer</p>
-        // </Swiper>
-        // <Swiper {...params}>
-        //     <img src='./img/attendee.png' alt="attendee" />
-        //     <img src='./img/trainer.png' alt="trainer" />
-        //     <img src='./img/management.png' alt="manager" />
-        //     <img src='./img/observer.png' alt="observer" />
-        // </Swiper> 
-        < Swiper {...params}>
-            <img src='./imgs/attendee.jpeg' alt="attendee" />
-            <img src='./imgs/trainer.jpeg' alt="trainer" />
-            <img src='./imgs/management.jpeg' alt="manager" />
-            <img src='./imgs/observer.jpeg' alt="observer" />
-        </Swiper >
-    )
-}
+const Slide = (props) => {
+	const params = {
+		pagination: {
+			el: '.swiper-pagination',
+			type: 'bullets',
+			clickable: true
+		},
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev'
+		},
+		slidesPerView: 'auto',
+		spaceBetween: 100,
+		loop: true,
+		centeredSlides: true,
+		on: {
+			slideChangeTransitionStart: props.swiperHandler
+		}
+	};
+	return (
+		<Swiper {...params}>
+			<img src="./img/attendee.png" alt="Attendee" />
+			<img src="./img/trainer.png" alt="Trainer" />
+			<img src="./img/management.png" alt="Manager" />
+			<img src="./img/observer.png" alt="Observer" />
+		</Swiper>
+	);
+};
 
 export default Slide;
