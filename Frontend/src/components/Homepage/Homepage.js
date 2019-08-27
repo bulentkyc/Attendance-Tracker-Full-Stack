543a2435e80a6ea6b96e9483a7d15d2ce259a8ad
Evgeny Borisov <45000799+Evborisov@users.noreply.github.com>
2019-08-27 14:45:14 +0200

    Update Homepage.js
    Added: passing props from app to dropdown menu

--- Frontend/src/components/Homepage/Homepage.js
+++ Frontend/src/components/Homepage/Homepage.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React from 'react';
+import DropdownMe from '../Dropdown/Dropdown';
 
-export default function Homepage() {
+function Homepage(props) {
+
+    //I am catching props with consolelog to be sure that I have them passed from App.js
+    console.log(props.forDropDown);
+
     return (
+
         <div>
             <h1>Hello from Home page</h1>
+            {
+                //I am passing props further to the next child component. My end point should be Dropdown.js component
+            }
+            <DropdownMe forChildDrop={props.forDropDown} />
         </div>
-    )
+    );
+
 }
+
+export default Homepage
 
